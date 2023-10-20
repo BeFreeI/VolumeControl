62c7cbc4e5c3e6627ee0a770f2149b4b063d293f
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-10-20 12:21:29 +0300

    Initial Commit

---
+++ VolumeControl/VolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeControlApp.swift
+//  VolumeControl
+//
+//  Created by  Pavel Nepogodin on 20.10.23.
+//
+
+import SwiftUI
+
+@main
+struct VolumeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
